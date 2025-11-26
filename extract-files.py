3539ff1724956f16b455dbc8ecea4d09d5c00d37
Andrey Sprynga <rtx4d@aospa.co>
2025-11-26 18:07:33 +0700

    check elf ?
    Change-Id: I6b0c38165ed6e038a6ab377561d6c0a1d782ee11

--- extract-files.py
+++ extract-files.py
@@ -22,7 +22,8 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/zte/akershus',
+    'device/smartisan/trident',
+    'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sdm845',
     'hardware/qcom-caf/wlan',
     'vendor/qcom/opensource/commonsys-intf/display',
@@ -74,24 +75,39 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libnativehelper_shim.so'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so'),
-    'vendor/lib/libZEffectLib.so': blob_fixup()
-        .add_needed('libui_shim.so'),
-    ('vendor/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service', 'vendor/lib64/vendor.qti.hardware.sensorscalibrate@1.0.so', 'vendor/lib64/com.fingerprints.extension@1.0.so'): blob_fixup()
-        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    ('vendor/lib/libtsvideoprocess.so', 'libswregistrationalgo'): blob_fixup()
+    ('vendor/lib/libmms_gyro_vstab_auth.so', 'vendor/lib/libmms_gyro_vstab.so', 'vendor/lib/libmms_hal_vstab.so', 'vendor/lib/libmms_warper_vstab.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    'vendor/lib/libFNVfbEngineHAL.so': blob_fixup()
-        .clear_symbol_version('AHardwareBuffer_allocate')
-        .clear_symbol_version('AHardwareBuffer_describe')
-        .clear_symbol_version('AHardwareBuffer_lock')
-        .clear_symbol_version('AHardwareBuffer_release')
-        .clear_symbol_version('AHardwareBuffer_unlock'),
-    'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
-        .clear_symbol_version('__aeabi_memcpy')
-        .clear_symbol_version('__aeabi_memset')
-        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
+    'vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti': blob_fixup()
+        .replace_needed('vendor.qti.hardware.fm@1.0_vendor.so', 'vendor.qti.hardware.fm@1.0.so')
+        .replace_needed('com.qualcomm.qti.ant@1.0_vendor.so', 'com.qualcomm.qti.ant@1.0.so'),
+    'system/lib/libimgcmp_jni.so': blob_fixup()
+        .remove_needed('libskia.so'),
+    'vendor/lib64/libnotifyaudiohal.so': blob_fixup()
+        .remove_needed('libicuuc.so'),
+    'system/lib/libmmcamera_dbg.so': blob_fixup()
+        .add_needed('liblog.so'),
+    'vendor/lib64/libgoodixfingerprintd_binder.so': blob_fixup()
+        .add_needed('libbinder_shim.so'),
+    ('vendor/lib64/vendor.goodix.hardware.fingerprintextension@1.0.so', 'vendor/lib64/com.fingerprints.extension@1.0.so', 'vendor/lib64/com.novatek.fingerprint@1.0_vendor.so', 'vendor/lib64/vendor.qti.hardware.fm@1.0.so', 'vendor/lib64/com.qualcomm.qti.bluetooth_audio@1.0_vendor.so', 'vendor/lib64/com.qualcomm.qti.ant@1.0.so', 'system_ext/lib64/com.qualcomm.qti.ant@1.0.so'): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    ('vendor/lib/libmms_warper_vstab.so', 'vendor/lib/libmms_hal_vstab.so'): blob_fixup()
+        .add_needed('libui_shim.so'),
+    ('system/lib/libarcsoft_beautyshot.so', 'system/lib/libarcsoft_dualcam_refocus.so', 'vendor/lib/libsns_low_lat_stream_stub.so', 'vendor/lib64/libsns_low_lat_stream_stub.so', 'vendor/lib/libssc_default_listener.so', 'vendor/lib64/libssc_default_listener.so'): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open'),
+    'vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so': blob_fixup()
+        .add_needed('libbase_shim.so'),
+    ('system/lib/libSMTSensorCalLibNative.so', 'system/lib64/libSMTSensorCalLibNative.so', 'system/lib/libsnsdiaglog.so', 'system/lib64/libsnsdiaglog.so', 'system/lib/libssc.so', 'system/lib64/libssc.so', 'vendor/lib/libsnsdiaglog.so', 'vendor/lib64/libsnsdiaglog.so', 'vendor/lib/libssc.so', 'vendor/lib64/libssc.so'): blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-v29'),
+    'vendor/lib/hw/camera.qcom.so': blob_fixup()
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open'),
+    'vendor/lib64/hw/com.qualcomm.qti.ant@1.0-impl.so': blob_fixup()
+        .replace_needed('com.qualcomm.qti.ant@1.0_vendor.so', 'com.qualcomm.qti.ant@1.0.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
