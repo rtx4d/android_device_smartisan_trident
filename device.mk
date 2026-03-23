91e0181bc7eb69a7d72287474d69bee95459fb59
Andrey Sprynga <rtx4d@aospa.co>
2026-03-23 20:41:16 +0700

    trident: Rebrand and build our core audio impl service
    Change-Id: I2a5b87084d6283068a883f982129489caf7acf5b

--- device.mk
+++ device.mk
@@ -86,7 +86,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio@6.0-impl.smartisan:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl:32 \
